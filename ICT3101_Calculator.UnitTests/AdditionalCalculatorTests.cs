58c813f3ff5fbcc0edf488ea025d28c1351cb9b3
bryanong120 <shadowfiend98@gmail.com>
2022-09-30 02:15:48 +0800

    Completed Step 14 of lab 4

--- ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
+++ ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
@@ -6,6 +6,23 @@ namespace ICT3101_Calculator.UnitTests;
 [TestFixture]
 public class AdditionalCalculatorTests
 {
-    // private Calculator _calculator;
-    // private Mock<IFileReader> _mockFileReader;
+    private Calculator _calculator;
+    private Mock<IFileReader> _mockFileReader;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockFileReader = new Mock<IFileReader>();
+        _mockFileReader.Setup(fr => fr.Read("MagicNumbers.txt")).Returns(new string[6] {"123", "123", "321", "23", "112", "232"});
+        _calculator = new Calculator();
+    }
+    
+    [Test]
+    public void GenMagicNum_GetNumber464Mock()
+    {
+        // Act
+        double result = _calculator.GenMagicNum(5, _mockFileReader.Object); 
+        // Assert
+        Assert.That(result, Is.EqualTo(464));
+    }
 }
